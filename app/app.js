83d314351595358b510d3dce7c96ec7360e53749
Zahra Ghanbari <ghanbarifam@gmail.com>
2017-05-09 16:00:53 +0200

    setting database and make connection

---
+++ app/app.js
@@ -0,0 +1,69 @@
+var fs = require('fs'),
+    express = require('express'),
+    app = express(),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    Sequelize = require('sequelize'),
+    pg = require('pg');
+    db = new Sequelize('zahra', 'zahra120', '', { dialect: 'postgres' });
+    app.use(function(req, res, next){
+      console.log(`${req.method} request for path '${req.url}' - ${req.body}`);
+      next();
+    });
+    app.use(express.static('/'));
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(morgan('dev'));
+
+    var User = db.define('user',{
+    firstName: {
+      type: Sequelize.STRING
+    },
+    lastName: {
+      type: Sequelize.STRING }
+    });
+
+    User.sync({force: true}).then(function(){
+      return User.create({
+        firstName:'zahra',
+        lastName: 'sa'
+
+      });
+    });
+    var Register = db.define('register', {
+        email: Sequelize.STRING,
+        password: Sequelize.STRING
+
+    });
+    // Register.sync({force: true}).then(function(){
+    //   return Register.create({});
+    // });
+
+
+app.get('/', function(req, res){
+  res.sendFile('/Users/zahra120/Desktop/app/app.html');
+});
+app.post('/', function(req, res){
+
+ var newUser = req.body;
+
+ Register.sync({force: true}).then(function(){
+  return Register.create(newUser).then(function(newUser){
+     res.redirect('/');
+   });
+ });
+
+
+
+
+ });
+
+
+
+
+
+
+
+
+  app.listen(3000, function() {
+    console.log('Web server started on port 3000');
+  });
